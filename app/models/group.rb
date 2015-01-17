9add8e180c74d8cae88f1784615b88addfb38a26
cody <cody.haind@gmail.com>
2015-01-17 13:25:50 +0700

    use subdomain validator & add validator test

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,4 @@
 class Group < ActiveRecord::Base
   validates :name, :subdomain, presence: true
+  validates :subdomain, subdomain: true
 end
--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -4,5 +4,10 @@ describe Group do
   describe 'validations' do
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_presence_of(:subdomain) }
+    it 'should have a valid subdomain' do
+      expect(FactoryGirl.build(:group, subdomain: 'www')).not_to be_valid
+      expect(FactoryGirl.build(:group, subdomain: 'speci@l')).not_to be_valid
+      expect(FactoryGirl.build(:group, subdomain: 'normal')).to be_valid
+    end
   end
 end
