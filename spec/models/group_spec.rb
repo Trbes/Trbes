7c3bbbe7814025906e0eea74322cdde4e8a3684d
Airat Shigapov <airat.shigapov@flatstack.com>
2015-01-17 22:53:22 +0300

    Use already included 'build' method in specs

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -5,9 +5,9 @@ describe Group do
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_presence_of(:subdomain) }
     it 'should have a valid subdomain' do
-      expect(FactoryGirl.build(:group, subdomain: 'www')).not_to be_valid
-      expect(FactoryGirl.build(:group, subdomain: 'speci@l')).not_to be_valid
-      expect(FactoryGirl.build(:group, subdomain: 'normal')).to be_valid
+      expect(build(:group, subdomain: 'www')).not_to be_valid
+      expect(build(:group, subdomain: 'speci@l')).not_to be_valid
+      expect(build(:group, subdomain: 'normal')).to be_valid
     end
   end
 end
