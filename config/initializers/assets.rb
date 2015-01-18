8d417812d4ae746c585659a8ffd9bee79cec7cb5
cody <cody.haind@gmail.com>
2015-01-19 01:49:07 +0700

    upgrade foundation overrides file & fix modernizr not working

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,6 +5,6 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( vendor/modernizr.js )
+Rails.application.config.assets.precompile += %w( modernizr/modernizr.js )
 
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components')
