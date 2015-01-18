b64225eb5c613b78d7227df8455fbe9cbb05d597
cody <cody.haind@gmail.com>
2015-01-19 01:56:49 +0700

    add nunito custom fontface to use as default san-serif

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,8 +3,10 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components')
+Rails.application.config.assets.paths << Rails.root.join("app", "assets", "fonts")
+
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w( modernizr/modernizr.js )
 
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components')
