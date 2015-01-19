2e28d99e0ff394476ae1bb836e246e3ef62060ff
Airat Shigapov <airat.shigapov@flatstack.com>
2015-01-19 22:52:00 +0300

    Fix indentation & hash syntax

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,13 +1,13 @@
 Rails.application.configure do
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
-    :enable_starttls_auto => true
+    address: 'smtp.sendgrid.net',
+    port: '587',
+    authentication: :plain,
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'heroku.com',
+    enable_starttls_auto: true
   }
 
   # Settings specified here will take precedence over those in config/application.rb.
