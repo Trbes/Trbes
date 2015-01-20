32f615236ce43ab56c25b274bf7db2e62f189c19
Airat Shigapov <airat.shigapov@flatstack.com>
2015-01-20 23:23:58 +0300

    Add prototype of different post types

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,13 @@
 class Post < ActiveRecord::Base
+  POSTABLE_TYPES = %w(
+    ImagePostable
+    LinkPostable
+    TextPostable
+  ).freeze
+
   belongs_to :group, required: true
   belongs_to :user, required: true
+  belongs_to :postable, polymorphic: true, required: true
 
   validates :title, presence: true
 end
