7a800c77d7b4710b13e82bef3fa39aa81a7675ea
Airat Shigapov <airat.shigapov@flatstack.com>
2015-01-29 21:26:20 +0300

    Move order scope to model

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,7 +2,7 @@ class GroupsController < ApplicationController
   before_action :ensure_group_is_loaded!, only: [:show]
 
   expose(:group, attributes: :group_attributes)
-  expose(:posts, only: [:show]) { current_group.posts.includes(:postable).order(cached_votes_total: :desc) }
+  expose(:posts, only: [:show]) { current_group.posts.includes(:postable).order_by_votes }
 
   def new
   end
--- app/models/post.rb
+++ app/models/post.rb
@@ -11,6 +11,8 @@ class Post < ActiveRecord::Base
 
   delegate :title, :body, :link, :image, :preview_image, to: :postable
 
+  scope :order_by_votes, -> { order(cached_votes_total: :desc) }
+
   accepts_nested_attributes_for :postable
 
   acts_as_votable
