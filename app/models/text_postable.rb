777b81da4930ef096d62c0516546ace458a043f1
Airat Shigapov <airat.shigapov@flatstack.com>
2015-02-02 16:49:53 +0300

    Add post preview images

--- app/models/text_postable.rb
+++ app/models/text_postable.rb
@@ -1,6 +1,13 @@
 class TextPostable < ActiveRecord::Base
+  default_scope { includes(:attachments) }
+
   has_one :post, as: :postable
+  has_many :attachments, as: :attachable
 
   validates :title, :body, presence: true
   validates :title, length: { minimum: 10, maximum: 100 }
+
+  def preview_image
+    attachments.first.image if attachments.any?
+  end
 end
