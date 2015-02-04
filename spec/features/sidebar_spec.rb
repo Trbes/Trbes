bf6b7fc1471685e3010a14ff472876f7c5d3a2f2
Airat Shigapov <airat.shigapov@flatstack.com>
2015-02-04 14:06:13 +0300

    Handle algolio properly by Webmock in specs

--- spec/features/sidebar_spec.rb
+++ spec/features/sidebar_spec.rb
@@ -1,17 +1,8 @@
-require "algolia/webmock"
 require "rails_helper"
 
 describe "With a mocked client" do
-  before(:each) do
-    WebMock.enable!
-  end
-
   it "shouldn't perform any API calls here" do
     create(:group) # mocked, no API call performed
     expect(Group.search("")).to be_empty # mocked, no API call performed
-  end
-
-  after(:each) do
-    WebMock.disable!
   end
 end
