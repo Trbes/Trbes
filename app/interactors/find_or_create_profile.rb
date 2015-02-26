a19a605ce603c47f28bb2f71b21913b7d6028c54
Airat Shigapov <airat.shigapov@flatstack.com>
2015-02-26 19:55:00 +0300

    Move oauth user creation to separate interactor, cleanup specs

--- app/interactors/find_or_create_profile.rb
+++ app/interactors/find_or_create_profile.rb
@@ -12,14 +12,7 @@ class FindOrCreateProfile
   end
 
   def create_profile
-    user = User.new(
-      full_name: auth_data.extra.raw_info.name,
-      password: Devise.friendly_token[0,20],
-      email: auth_data.info.email || "#{auth_data.info.nickname}@#{auth_data.provider}.com"
-    )
-
-    user.skip_confirmation!
-    user.save!
+    user = CreateUserFromOauth.call(auth_data: auth_data).user
 
     Profile.create(uid: auth_data.uid, provider: auth_data.provider, user: user)
   end
