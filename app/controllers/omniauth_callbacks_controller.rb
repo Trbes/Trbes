789be225369b965122c0d2661e06d5fd808cfd41
Airat Shigapov <airat.shigapov@flatstack.com>
2015-02-26 19:29:46 +0300

    Move profile find/create logic to interactor

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,6 +1,6 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def twitter
-    profile = Profile.find_for_oauth(env["omniauth.auth"], current_user)
+    profile = FindOrCreateProfile.call(auth_data: env["omniauth.auth"]).profile
 
     sign_in_and_redirect profile.user, event: :authentication
     set_flash_message(:notice, :success, kind: "#{profile.provider}".capitalize)
---
+++ app/interactors/find_or_create_profile.rb
@@ -0,0 +1,30 @@
+class FindOrCreateProfile
+  include Interactor
+
+  def call
+    context.profile = find_profile || create_profile
+  end
+
+  private
+
+  def find_profile
+    Profile.find_by(uid: auth_data.uid, provider: auth_data.provider)
+  end
+
+  def create_profile
+    user = User.new(
+      full_name: auth_data.extra.raw_info.name,
+      password: Devise.friendly_token[0,20],
+      email: auth_data.email || "#{auth_data.info.nickname}@#{auth_data.provider}.com"
+    )
+
+    user.skip_confirmation!
+    user.save!
+
+    Profile.create(uid: auth_data.uid, provider: auth_data.provider, user: user)
+  end
+
+  def auth_data
+    context.auth_data
+  end
+end
