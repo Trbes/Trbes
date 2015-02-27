f92ae9f447f3782f7f6edbeb84a30682a1cdd660
Airat Shigapov <airat.shigapov@flatstack.com>
2015-02-27 18:02:04 +0300

    Cleanup OmniauthCallbacks controller

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,26 +1,23 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def twitter
-    profile = FindOrCreateProfile.call(auth_data: env["omniauth.auth"]).profile
-
-    sign_in_and_redirect profile.user, event: :authentication
-    set_flash_message(:notice, :success, kind: "#{profile.provider}".capitalize)
+    social_sign_in
   end
 
   def facebook
-    profile = FindOrCreateProfile.call(auth_data: env["omniauth.auth"]).profile
-
-    sign_in_and_redirect profile.user, event: :authentication
-    set_flash_message(:notice, :success, kind: "#{profile.provider}".capitalize)
+    social_sign_in
   end
 
   def google_oauth2
-    profile = FindOrCreateProfile.call(auth_data: env["omniauth.auth"]).profile
-
-    sign_in_and_redirect profile.user, event: :authentication
-    set_flash_message(:notice, :success, kind: "#{profile.provider}".capitalize)
+    social_sign_in
   end
 
   def linkedin
+    social_sign_in
+  end
+
+  private
+
+  def social_sign_in
     profile = FindOrCreateProfile.call(auth_data: env["omniauth.auth"]).profile
 
     sign_in_and_redirect profile.user, event: :authentication
