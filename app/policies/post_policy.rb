f1c519cfd2938928f5b92b45f3f96357258ee811
Airat Shigapov <airat.shigapov@flatstack.com>
2015-03-05 16:46:35 +0300

    Don't highlight post when guest tries to vote

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -2,4 +2,8 @@ class PostPolicy < Struct.new(:membership, :post)
   def publish?
     membership && (membership.owner? || membership.moderator? || membership.member?)
   end
+
+  def vote?(user)
+    user
+  end
 end
