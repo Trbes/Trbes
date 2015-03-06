843a4d41bbfdad6672e75558b998ec9a4447a91c
Airat Shigapov <airat.shigapov@flatstack.com>
2015-03-06 18:41:08 +0300

    Ensure that moderators and admins always can edit post

--- spec/features/posts/edit_spec.rb
+++ spec/features/posts/edit_spec.rb
@@ -1,5 +1,33 @@
 require "rails_helper"
 
+shared_examples_for "editable posts" do
+  scenario "I can see 'edit' link on posts list page" do
+    visit root_path
+
+    within("#post_#{post.id}") do
+      expect(page).to have_link("Edit")
+    end
+  end
+
+  scenario "I can see 'edit' link on single post page" do
+    visit post_path(post)
+
+    expect(page).to have_link("Edit post")
+  end
+
+  scenario "I can visit 'edit' post page" do
+    visit edit_post_path(post)
+
+    expect(page).to have_css("form.edit_post")
+
+    fill_in "Title", with: "Some new title"
+
+    expect {
+      click_button "Save"
+    }.to change { post.reload.title }.to("Some new title")
+  end
+end
+
 feature "Edit post" do
   let(:user) { create(:user, :confirmed) }
   let(:group) { create(:group) }
@@ -16,31 +44,7 @@ feature "Edit post" do
       post.update_attribute(:created_at, 1.minute.ago)
     end
 
-    scenario "I can see 'edit' link on posts list page" do
-      visit root_path
-
-      within("#post_#{post.id}") do
-        expect(page).to have_link("Edit")
-      end
-    end
-
-    scenario "I can see 'edit' link on single post page" do
-      visit post_path(post)
-
-      expect(page).to have_link("Edit post")
-    end
-
-    scenario "I can visit 'edit' post page" do
-      visit edit_post_path(post)
-
-      expect(page).to have_css("form.edit_post")
-
-      fill_in "Title", with: "Some new title"
-
-      expect {
-        click_button "Save"
-      }.to change { post.reload.title }.to("Some new title")
-    end
+    it_behaves_like "editable posts"
   end
 
   context "when post is not available for edit" do
@@ -48,24 +52,42 @@ feature "Edit post" do
       post.update_attribute(:created_at, 20.minutes.ago)
     end
 
-    scenario "I can't see 'edit' link on posts list page" do
-      visit root_path
+    context "when I'm group member" do
+      scenario "I can't see 'edit' link on posts list page" do
+        visit root_path
 
-      within("#post_#{post.id}") do
-        expect(page).not_to have_link("Edit")
+        within("#post_#{post.id}") do
+          expect(page).not_to have_link("Edit")
+        end
+      end
+
+      scenario "I can't see 'edit' link on single post page" do
+        visit post_path(post)
+
+        expect(page).not_to have_link("Edit post")
+      end
+
+      scenario "I can't visit 'edit' post page" do
+        visit edit_post_path(post)
+
+        expect(page).to have_content("You are not authorized to perform this action.")
       end
     end
 
-    scenario "I can't see 'edit' link on single post page" do
-      visit post_path(post)
+    context "when I'm group moderator" do
+      background do
+        user.membership_for(group).moderator!
+      end
 
-      expect(page).not_to have_link("Edit post")
+      it_behaves_like "editable posts"
     end
 
-    scenario "I can't visit 'edit' post page", js: true do
-      visit edit_post_path(post)
+    context "when I'm group owner" do
+      background do
+        user.membership_for(group).owner!
+      end
 
-      expect(page).to have_content("You are not authorized to perform this action.")
+      it_behaves_like "editable posts"
     end
   end
 end
