9c90d18eb5c0d5f84c26135826206d5f42c19f89
Cody Nguyen <cody.haind@gmail.com>
2015-03-10 13:20:31 +0700

    show share link under vote button; show author avatar on the right; show user popup when clicking any user name / avatar

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -26,4 +26,16 @@ module PostsHelper
     opts.each { |key, value| params << "#{key}=#{ URI.encode(value) }" }
     "https://twitter.com/intent/tweet?" + params.join("&")
   end
+
+  def user_popover_content(user)
+    html = "<div class='user-popover-content'>"
+    html += image_tag(user.avatar, class: "img-circle", width: 64, height: 64)
+    html += "<div class='user-name'>#{user.full_name}</div>"
+    html += "<div class='user-title'>#{user.title}</div>" if user.title.present?
+    html += "</div>"
+  end
+
+  def user_popover_template
+    '<div class="popover popover-user" role="tooltip"><div class="arrow"></div><h3 class="popover-title"></h3><div class="popover-content"></div></div>'
+  end
 end
