9021fc81bae8c7c6e80e0074f52b44e33fc3bfcd
Cody Nguyen <cody.haind@gmail.com>
2015-03-11 23:58:04 +0700

    move post meta to the right in posts listing, leave mod action links in post bottom

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -30,4 +30,22 @@ module PostsHelper
   def sort_class(filter)
     params[:sort] == filter ? "active" : ""
   end
+
+  def short_time_distance_string(time)
+    a = (Time.now - time).to_i
+
+    case a
+      when 0 then '1s'
+      when 1 then '1s'
+      when 2..59 then a.to_s + 's'
+      when 60..119 then '1m' #120 = 2 minutes
+      when 120..3540 then (a / 60).to_i.to_s + 'm'
+      when 3541..7100 then '1h' # 3600 = 1 hour
+      when 7101..82800 then ((a + 99) / 3600).to_i.to_s + 'h'
+      when 82801..172000 then '1d' # 86400 = 1 day
+      when 172001..518400 then ((a + 800) / (60 * 60 * 24)).to_i.to_s + 'd'
+      when 518400..1036800 then '1w'
+      else ((a + 180000) / (60 * 60 * 24 * 7)).to_i.to_s + 'w'
+    end
+  end
 end
