8efb5a084aad79f6ce64f93e87eaa3154495372d
Cody Nguyen <cody.haind@gmail.com>
2015-03-12 00:08:25 +0700

    ensure quality & update specs

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -31,21 +31,23 @@ module PostsHelper
     params[:sort] == filter ? "active" : ""
   end
 
+  # rubocop:disable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/MethodLength
   def short_time_distance_string(time)
     a = (Time.now - time).to_i
 
     case a
-      when 0 then '1s'
-      when 1 then '1s'
-      when 2..59 then a.to_s + 's'
-      when 60..119 then '1m' #120 = 2 minutes
-      when 120..3540 then (a / 60).to_i.to_s + 'm'
-      when 3541..7100 then '1h' # 3600 = 1 hour
-      when 7101..82800 then ((a + 99) / 3600).to_i.to_s + 'h'
-      when 82801..172000 then '1d' # 86400 = 1 day
-      when 172001..518400 then ((a + 800) / (60 * 60 * 24)).to_i.to_s + 'd'
-      when 518400..1036800 then '1w'
-      else ((a + 180000) / (60 * 60 * 24 * 7)).to_i.to_s + 'w'
+    when 0 then "1s"
+    when 1 then "1s"
+    when 2..59 then a.to_s + "s"
+    when 60..119 then "1m" # 120 = 2 minutes
+    when 120..3_540 then (a / 60).to_i.to_s + "m"
+    when 3_541..7_100 then "1h" # 3600 = 1 hour
+    when 7_101..82_800 then ((a + 99) / 3_600).to_i.to_s + "h"
+    when 82_801..172_000 then "1d" # 86400 = 1 day
+    when 172_001..518_400 then ((a + 800) / (60 * 60 * 24)).to_i.to_s + "d"
+    when 518_400..1_036_800 then "1w"
+    else ((a + 180_000) / (60 * 60 * 24 * 7)).to_i.to_s + "w"
     end
   end
+  # rubocop:enable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/MethodLength
 end
