c8b8e20962ed7df0a58cd26331304261210b2be2
Airat Shigapov <airat.shigapov@flatstack.com>
2015-03-11 17:40:55 +0300

    Add spec for 'Trending' sort

--- spec/features/posts/sort_spec.rb
+++ spec/features/posts/sort_spec.rb
@@ -5,7 +5,7 @@ feature "Sort posts list" do
   let(:group) { create(:group, users: [user]) }
   let!(:old_popular_post) { create(:post, :published, created_at: 5.days.ago, cached_votes_total: 100, group: group) }
   let!(:fresh_post) { create(:post, :published, created_at: DateTime.now, cached_votes_total: 50, group: group) }
-  let!(:popular_but_not_so_fresh_post) { create(:post, :published, created_at: 1.day.ago, cached_votes_total: 150, group: group) }
+  let!(:popular_but_not_so_fresh_post) { create(:post, :published, created_at: 4.hours.ago, cached_votes_total: 250, group: group) }
 
   background do
     switch_to_subdomain(group.subdomain)
@@ -28,6 +28,14 @@ feature "Sort posts list" do
 
     expect(page.all(".post .title")[0].text).to eq(fresh_post.title)
     expect(page.all(".post .title")[1].text).to eq(popular_but_not_so_fresh_post.title)
+    expect(page.all(".post .title")[2].text).to eq(old_popular_post.title)
+  end
+
+  scenario "Sort by 'Trending'" do
+    click_link "Trending"
+
+    expect(page.all(".post .title")[0].text).to eq(popular_but_not_so_fresh_post.title)
+    expect(page.all(".post .title")[1].text).to eq(fresh_post.title)
     expect(page.all(".post .title")[2].text).to eq(old_popular_post.title)
   end
 end
