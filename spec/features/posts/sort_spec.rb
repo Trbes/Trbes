c2170bc156940b7bd8b0c3f7c06b44a837c8ddf5
Airat Shigapov <airat.shigapov@flatstack.com>
2015-03-11 17:34:10 +0300

    Add spec for 'New' sort

--- spec/features/posts/sort_spec.rb
+++ spec/features/posts/sort_spec.rb
@@ -5,7 +5,7 @@ feature "Sort posts list" do
   let(:group) { create(:group, users: [user]) }
   let!(:old_popular_post) { create(:post, :published, created_at: 5.days.ago, cached_votes_total: 100, group: group) }
   let!(:fresh_post) { create(:post, :published, created_at: DateTime.now, cached_votes_total: 50, group: group) }
-  let!(:popular_but_not_fresh_post) { create(:post, :published, created_at: 1.day.ago, cached_votes_total: 150, group: group) }
+  let!(:popular_but_not_so_fresh_post) { create(:post, :published, created_at: 1.day.ago, cached_votes_total: 150, group: group) }
 
   background do
     switch_to_subdomain(group.subdomain)
@@ -18,12 +18,16 @@ feature "Sort posts list" do
   scenario "Sort by 'Popular'" do
     click_link "Popular"
 
-    expect(page.all(".post .title")[0].text).to eq(popular_but_not_fresh_post.title)
+    expect(page.all(".post .title")[0].text).to eq(popular_but_not_so_fresh_post.title)
     expect(page.all(".post .title")[1].text).to eq(old_popular_post.title)
     expect(page.all(".post .title")[2].text).to eq(fresh_post.title)
   end
 
   scenario "Sort by 'New'" do
+    click_link "New"
 
+    expect(page.all(".post .title")[0].text).to eq(fresh_post.title)
+    expect(page.all(".post .title")[1].text).to eq(popular_but_not_so_fresh_post.title)
+    expect(page.all(".post .title")[2].text).to eq(old_popular_post.title)
   end
 end
