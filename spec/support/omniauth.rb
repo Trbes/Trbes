9673895e1283580830f5d965e9e6c0b2ba1b04b9
Airat Shigapov <airat.shigapov@flatstack.com>
2015-03-12 18:13:30 +0300

    Fix omniauth mocks to match new expected response format

--- spec/support/omniauth.rb
+++ spec/support/omniauth.rb
@@ -3,22 +3,19 @@ OmniAuth.config.test_mode = true
 OmniAuth.config.add_mock(:twitter, {
   provider: "twitter",
   uid: "12345",
-  info: { nickname: "name" },
-  extra: { raw_info: { name: "name" } }
+  info: { name: "name", nickname: "name" }
 })
 
 OmniAuth.config.add_mock(:facebook, {
   provider: "facebook",
   uid: "12345",
-  info: { email: "user@example.com" },
-  extra: { raw_info: { name: "name" } }
+  info: { name: "name", email: "user@example.com" },
 })
 
 OmniAuth.config.add_mock(:google_oauth2, {
   provider: "google_oauth2",
   uid: "12345",
-  info: { email: "user@example.com" },
-  extra: { raw_info: { name: "name" } }
+  info: { name: "name", email: "user@example.com" },
 })
 
 OmniAuth.config.add_mock(:linkedin, {
@@ -27,6 +24,5 @@ OmniAuth.config.add_mock(:linkedin, {
   info: {
     email: "user@example.com",
     name: "name"
-  },
-  extra: { raw_info: { name: "name" } }
+  }
 })
