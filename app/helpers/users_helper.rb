cda4925ea1bfce65d6e0f774a0e81c5a155499bb
Cody Nguyen <cody.haind@gmail.com>
2015-03-18 00:24:39 +0700

    add role overlay to user avatar; add role title to user popover

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,11 @@
 module UsersHelper
   def user_popover_content(user)
+    role = current_group && (membership = user.membership_for(current_group)) ? membership.role : nil
+
     html = "<div class='user-popover-content'>"
+    if role
+      html += "<div class='user-role user-role-#{role}'>group #{role}</div>"
+    end
     html += image_tag(user.avatar, class: "img-circle", width: 64, height: 64)
     html += "<div class='user-name'>#{user.full_name}</div>"
     html += "<div class='user-title'>#{user.title}</div>" if user.title.present?
