cc947a47535b24bf0923617520bc58379951c3e3
Cody Nguyen <cody.haind@gmail.com>
2015-03-21 00:14:22 +0700

    fix bug causing exception in user edit form with avatar field

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,9 @@ class User < ActiveRecord::Base
   has_many :profiles, dependent: :destroy
 
   has_one :avatar, as: :attachable, class_name: "Attachment"
+  accepts_nested_attributes_for :avatar, update_only: true
+
+  delegate :image, to: :avatar, prefix: true, allow_nil: true
 
   validates :full_name, presence: true
 
@@ -18,8 +21,8 @@ class User < ActiveRecord::Base
     memberships.for_group(group).first
   end
 
-  def avatar
-    super || "http://gravatar.com/avatar/#{Digest::MD5.hexdigest(email.downcase)}.png?d=mm"
+  def avatar_url
+    avatar_image.try(:thumbnail) || "http://gravatar.com/avatar/#{Digest::MD5.hexdigest(email.downcase)}.png?d=mm"
   end
 
   def full_name
