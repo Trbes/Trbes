8c669b90782ca15c947f943aa320ac6ba3cf0922
Cody Nguyen <cody.haind@gmail.com>
2015-03-24 00:35:11 +0700

    quality ensurance

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,11 @@
 class SessionsController < Devise::SessionsController
-  after_filter :after_login, :only => :create
+  after_action :after_login, only: :create
 
   def after_login
-    if current_group && (pending_memberships_count = current_group.memberships.pending.count) > 0 && (current_membership.owner? || current_membership.moderator?)
-      flash[:alert] = I18n.t("app.group.message.pending_members", count: pending_memberships_count)
-    end
+    return unless current_group &&
+        (pending_memberships_count = current_group.memberships.pending.count) > 0 &&
+        (current_membership.owner? || current_membership.moderator?)
+
+    flash[:alert] = I18n.t("app.group.message.pending_members", count: pending_memberships_count)
   end
 end
