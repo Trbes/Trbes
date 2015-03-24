1c564130d25d5d47bd594d10aa0a440c74578325
Airat Shigapov <airat.shigapov@flatstack.com>
2015-03-24 18:12:36 +0300

    Cover comments editing with specs

---
+++ spec/features/admin/comments/edit_spec.rb
@@ -0,0 +1,27 @@
+require "rails_helper"
+
+feature "Edit comment from admin section" do
+  include_context "group membership and authentication"
+
+  let(:post) { create(:post, :published, group: group) }
+  let!(:comment) { create(:comment, :published, post: post) }
+
+  before do
+    membership.owner!
+  end
+
+  scenario "Edit comment", js: true do
+    visit admin_comments_path
+
+    within("#comment_#{comment.id}") do
+      click_link "edit"
+    end
+
+    fill_in "comment_body", with: "New body"
+    click_button "Save"
+
+    within("#comment_#{comment.id}") do
+      expect(page).to have_content("New body")
+    end
+  end
+end
