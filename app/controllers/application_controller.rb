d8346a7142601ec01fbd4c5b21193e6439e1fb78
Airat Shigapov <airat.shigapov@flatstack.com>
2015-03-27 11:50:41 +0300

    Update rest-client, fix specs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,8 +13,7 @@ class ApplicationController < ActionController::Base
 
   helper_method :current_group
   def current_group
-    @current_group ||= view_context
-      .present(Group.includes(:logo, memberships: { user: :avatar }).find_by(subdomain: request.subdomain))
+    @current_group ||= Group.includes(:logo, memberships: { user: :avatar }).find_by(subdomain: request.subdomain)
   end
 
   helper_method :current_membership
