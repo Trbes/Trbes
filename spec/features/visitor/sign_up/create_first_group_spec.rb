797c8e9930c784bceb1a444ee94f9eac62cb0191
Airat Shigapov <contact@airatshigapov.com>
2015-03-31 16:31:30 +0300

    Cleanup visitor specs

--- spec/features/visitor/sign_up/create_first_group_spec.rb
+++ spec/features/visitor/sign_up/create_first_group_spec.rb
@@ -1,49 +1,53 @@
 require "rails_helper"
 
 feature "Create first group" do
-  let(:user) { create(:user) }
+  include_context "group membership and authentication"
+
   let(:group_attributes) { attributes_for(:group) }
-  let(:group) { Group.first }
+  let(:created_group) { Group.last }
 
   context "Unauthorized" do
+    background do
+      click_link "Sign out"
+    end
+
     scenario "Unauthorized user tries to create group" do
       visit welcome_path
+
       expect(page.current_path).to eql root_path
     end
   end
 
   context "Authorized" do
-    before(:each) do
+    background do
+      group.destroy
+      switch_to_main
       sign_in(user.email, "123456")
+    end
+
+    scenario "User create first group successfully" do
       visit welcome_path
 
       fill_in "Group Name", with: group_attributes[:name]
       fill_in "Short Name", with: group_attributes[:subdomain]
       fill_in "Tagline", with: group_attributes[:tagline]
       fill_in "Description", with: group_attributes[:description]
-    end
 
-    after(:each) do
-      switch_to_main
-    end
-
-    scenario "User create first group successfully" do
       click_button "Create Trbes Group"
 
       # Redirect to group invite url
       expect(page.current_path).to eql new_invitation_path
-      expect(URI.parse(page.current_url).host.match(/([^\.]*)\..*$/)[1]).to eql group.subdomain
+      expect(current_url).to include(created_group.reload.subdomain)
 
       # Group is created
-      expect(group).to be_persisted
-      expect(group.users).to include(user)
-      expect(group.allow_text_posts).to be true
-      expect(group.allow_link_posts).to be true
-      expect(group.allow_image_posts).to be true
+      expect(created_group).to be_persisted
+      expect(created_group.users).to include(user)
+      expect(created_group.allow_text_posts).to be_truthy
+      expect(created_group.allow_link_posts).to be_truthy
+      expect(created_group.allow_image_posts).to be_truthy
 
       # User is added as owner
-      membership = group.memberships.find_by(user: user)
-      expect(membership.owner?).to be true
+      expect(user.membership_for(created_group)).to be_owner
     end
   end
 end
