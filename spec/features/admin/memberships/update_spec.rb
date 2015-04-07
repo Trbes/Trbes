4640476510bde4c60e1398ce4ebfdc18cd553ccf
Airat Shigapov <contact@airatshigapov.com>
2015-04-07 14:53:58 +0300

    Cover automatic posts publishing with specs

--- spec/features/admin/memberships/update_spec.rb
+++ spec/features/admin/memberships/update_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-feature "Update membership" do
+feature "Update membership", js: true do
   include_context "group membership and authentication"
 
   let!(:memberships) { create_list(:membership, 5, role: :member, group: group) }
@@ -11,7 +11,7 @@ feature "Update membership" do
     visit admin_memberships_path
   end
 
-  scenario "I can change member role within members list", js: true do
+  scenario "I can change member role within members list" do
     find("section.memberships #membership_#{some_membership.id} .role-link").click
 
     within("ul.select2-results") do
@@ -30,7 +30,43 @@ feature "Update membership" do
     expect(current_email).to have_subject "Your role in #{group.name} has changed"
   end
 
-  scenario "I can transfer ownership", js: true do
+  context "when there are posts by this user in 'moderation' state" do
+    background do
+      some_membership.pending!
+      create_list(:post, 3, user: some_membership.user, group: group, state: :moderation)
+
+      visit admin_memberships_path
+    end
+
+    scenario "posts are published after membership is confirmed" do
+      group.posts.for_user(some_membership.user).each do |post|
+        expect(post).to be_moderation
+      end
+
+      find("section.memberships #membership_#{some_membership.id} .role-link").click
+
+      within("ul.select2-results") do
+        find("li div", text: /\Amember\z/).click
+      end
+
+      wait_for_ajax
+
+      within("section.memberships #membership_#{some_membership.id}") do
+        expect(page).to have_content("member")
+        expect(some_membership.reload.role).to eq("member")
+      end
+
+      open_email(some_membership.email)
+
+      expect(current_email).to have_subject "Your role in #{group.name} has changed"
+
+      group.posts.for_user(some_membership.user).each do |post|
+        expect(post).to be_published
+      end
+    end
+  end
+
+  scenario "I can transfer ownership" do
     click_link("owner")
 
     select(some_membership.full_name, from: "membership_new_group_owner_id")
