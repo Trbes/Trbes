de67df67b9ee486b24ac7282235f8e78a66f2f21
Airat Shigapov <contact@airatshigapov.com>
2015-04-10 12:23:58 +0300

    Make group authorization global

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
 
   rescue_from Pundit::NotAuthorizedError, with: :not_authorized
 
-  before_action :push_algolia_config, :push_env_config, :push_indexes, :ensure_email_is_exists
+  before_action :push_algolia_config, :push_env_config, :push_indexes, :ensure_email_is_exists, :authorize_group_access
 
   expose(:groups)
   expose(:group_memberships) { current_group.memberships.joins(:user).confirmed.not_pending }
@@ -26,6 +26,10 @@ class ApplicationController < ActionController::Base
     return unless current_group && current_user
 
     @current_membership ||= current_user.membership_for(current_group)
+  end
+
+  def authorize_group_access
+    authorize(current_group) if current_group
   end
 
   def ensure_group_access_from_canonical_url!
--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -33,7 +33,6 @@ class GroupsController < ApplicationController
   end
 
   def show
-    authorize(current_group)
     @group_name = current_group.name
     @group_tagline = current_group.tagline
     # So that search result click will redirects to group's url
