897adcf8c75c62100a253952742aaa553a507ccd
Airat Shigapov <contact@airatshigapov.com>
2015-04-14 19:23:22 +0300

    Add todo notice about flash messages

--- app/interactors/update_collection.rb
+++ app/interactors/update_collection.rb
@@ -19,6 +19,7 @@ class UpdateCollection
     context.collection.save
   end
 
+  # TODO: Extract this messages setting logic to separate composite (preferred) or parent class
   def set_success_message
     context.message = %(Collection "#{collection.name}" was successfully updated)
   end
