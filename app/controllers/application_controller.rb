9a0ad58a75cfe9ab085a1e81313df31a70636024
Airat Shigapov <contact@airatshigapov.com>
2015-04-17 16:41:56 +0300

    Cleanup gon-related before_actions, simplify current_group, group_memberships -> current_group_memberships

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,21 +9,19 @@ class ApplicationController < ActionController::Base
 
   rescue_from Pundit::NotAuthorizedError, with: :not_authorized
 
-  before_action :push_algolia_config, :push_env_config, :push_indexes, :ensure_email_is_exists, :authorize_group_access
+  before_action :push_gon_config, :push_indexes, :ensure_email_is_exists, :authorize_group_access
 
   expose(:groups)
-  expose(:group_memberships) { current_group.memberships.joins(:user).confirmed.not_pending }
+  expose(:current_group_memberships) { current_group.memberships.joins(:user).confirmed.not_pending }
   expose(:current_user_memberships) { current_user.memberships.includes(:group) }
 
   helper_method :current_group
   def current_group
-    matching_groups = if request.host.include?(Trbes::Application.config.host)
-      Group.where(subdomain: request.subdomain)
+    @current_group ||= if request.host.include?(Trbes::Application.config.host)
+      Group.includes(memberships: :user).find_by(subdomain: request.subdomain)
     else
-      Group.where(custom_domain: request.domain)
+      Group.includes(memberships: :user).find_by(custom_domain: request.domain)
     end
-
-    @current_group ||= matching_groups.includes(memberships: :user).first
   end
 
   helper_method :current_membership
@@ -105,7 +103,8 @@ class ApplicationController < ActionController::Base
     )
   end
 
-  def push_env_config
+  def push_gon_config
+    push_algolia_config
     gon.push(zero_clipboard_path: view_context.asset_path("zeroclipboard/ZeroClipboard.swf"))
     gon.push(facebook_app_id: ENV["FACEBOOK_APP_ID"])
   end
