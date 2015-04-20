917201a0240699ba5a82eece01305df63fb4c21d
Airat Shigapov <contact@airatshigapov.com>
2015-04-20 18:53:47 +0300

    Add touch: true to some posts/groups associations so it doesn't break caching

--- app/models/collection_post.rb
+++ app/models/collection_post.rb
@@ -4,7 +4,7 @@ class CollectionPost < ActiveRecord::Base
   scope :ordered, -> { rank(:row_order) }
   scope :for_post, -> (post) { where(post_id: post.id) }
 
-  belongs_to :post, required: true
+  belongs_to :post, required: true, touch: true
   belongs_to :collection, required: true, counter_cache: true, inverse_of: :collection_posts
 
   delegate :image, :name, to: :collection, prefix: true
