8e9d81550fd3a623b9277089a4c8822d3530677d
Cody Nguyen <cody.haind@gmail.com>
2015-04-22 00:39:05 +0700

    align link favicon to top when title is on 2+ lines

--- app/assets/javascripts/grabicon.js
+++ app/assets/javascripts/grabicon.js
@@ -4,7 +4,7 @@ function iconify_links() {
             var link = $(this).data("iconify-target");
             var size = parseInt($(this).data("iconify-size")) || 16;
             $(this).css({
-                background: "url(http://grabicon.com/icon?size=" + size + "&origin=" + window.location.host + "&domain=" + link + ") left center no-repeat",
+                background: "url(http://grabicon.com/icon?size=" + size + "&origin=" + window.location.host + "&domain=" + link + ") 0px 5px no-repeat",
                 "padding-left": Math.round(size + 4) + "px"
             });
         });
