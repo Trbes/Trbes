a0adcc7cdf7dcf926fc93ee6db1f6b02b6e32b8e
Airat Shigapov <contact@airatshigapov.com>
2015-04-24 18:28:34 +0300

    Get rid of MembershipPolicy and extra partial, move role_overlay to membership presenter

--- app/policies/membership_policy.rb
+++
@@ -1,5 +0,0 @@
-class MembershipPolicy < Struct.new(:current_membership, :membership)
-  def show_role_overlay?
-    membership && (membership.moderator? || membership.owner?)
-  end
-end
--- app/presenters/membership_presenter.rb
+++ app/presenters/membership_presenter.rb
@@ -1,4 +1,16 @@
 class MembershipPresenter < BasePresenter
+  def show_role_overlay?
+    moderator? || owner?
+  end
+
+  def role_overlay
+    return unless show_role_overlay?
+
+    h.content_tag(:span, class: "role-overlay role-overlay-#{role}") do
+      role_letter
+    end
+  end
+
   def role_letter
     role[0]
   end
