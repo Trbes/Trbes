2f4218a8d82f75cdbdf8cb225165dbfaae447b24
Cody Nguyen <cody.haind@gmail.com>
2015-05-07 00:23:24 +0700

    allow users & moderators to unsubscribe

--- spec/features/sidebar_spec.rb
+++ spec/features/sidebar_spec.rb
@@ -1,13 +1,13 @@
 require "rails_helper"
 
-feature "Sidebar" do
+feature "Sidebar", driver: :webkit do
   include_context "group membership and authentication"
 
   background do
     visit root_path
   end
 
-  scenario "Search for groups", js: true, driver: :webkit do
+  scenario "Search for groups", js: true do
     page.find(".navbar-brand").trigger("click")
 
     within(".tt-dropdown-menu", visible: false) do
@@ -55,6 +55,17 @@ feature "Sidebar" do
           expect(page).not_to have_content(other_group.name.upcase)
         end
       end
+    end
+
+    scenario "Unsubscribe from group", js: true do
+      visit root_path
+
+      page.find(".navbar-brand").trigger("click")
+      within("#main_sliding_menu #sidebar_group_#{group.id}") do
+        page.find(".unsubscribe", visible: false).trigger("click")
+      end
+
+      expect(page).not_to have_css("#main_sliding_menu #sidebar_group_#{group.id}", visible: false)
     end
   end
 end
