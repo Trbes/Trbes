022d133f7bed5c62714e345efd9c72f48681edd9
Airat Shigapov <contact@airatshigapov.com>
2015-05-14 19:12:08 +0300

    Add spec for weekly digest when there are no new posts

--- spec/mailers/group_mailer_spec.rb
+++ spec/mailers/group_mailer_spec.rb
@@ -8,9 +8,22 @@ describe GroupMailer do
   let(:mail) { described_class.weekly_digest_email(membership) }
 
   describe "#weekly_digest_email" do
-    it "contains posts titles" do
-      posts.each do |post|
-        expect(mail.body.encoded).to include(post.title)
+    context "when there are new posts" do
+      it "contains posts titles" do
+        posts.each do |post|
+          expect(mail.body.encoded).to include(post.title)
+        end
+      end
+    end
+
+    context "when there are no new posts" do
+      before do
+        Post.update_all(created_at: 2.weeks.ago)
+      end
+
+      it "contains link to site" do
+        expect(mail.body.encoded).to include("There are no new posts in the past week")
+        expect(mail.body.encoded).to include("http://#{group.subdomain}.#{DEFAULT_HOST}")
       end
     end
   end
