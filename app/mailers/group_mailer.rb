7bde7d998766a1cb240df22dd21fccaffca5039b
Airat Shigapov <contact@airatshigapov.com>
2015-05-14 19:11:54 +0300

    Send different text when there are no new posts

--- app/mailers/group_mailer.rb
+++ app/mailers/group_mailer.rb
@@ -2,9 +2,11 @@ class GroupMailer < ApplicationMailer
   layout "mailer"
 
   helper ApplicationHelper
+  helper GroupsHelper
 
   def weekly_digest_email(membership)
     @membership = membership
+    @posts = @membership.group.posts.for_weekly_digest
 
     mail(to: membership.user_email, subject: "Weekly digest for #{@membership.group_name}")
   end
