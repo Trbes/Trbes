e209b70c7f24575234e3049662186c6c3f32d346
Airat Shigapov <contact@airatshigapov.com>
2015-05-15 11:06:26 +0300

    Use grabicon with API key since trial is over

--- app/assets/javascripts/grabicon.js
+++ app/assets/javascripts/grabicon.js
@@ -2,7 +2,7 @@ function iconify_link(elem) {
     var link = elem.data("iconify-target");
     var size = parseInt(elem.data("iconify-size")) || 16;
     elem.css({
-        background: "url(http://grabicon.com/icon?size=" + size + "&origin=" + window.location.host + "&domain=" + link + ") 0px 5px no-repeat",
+        background: "url(http://grabicon.com/icon?size=" + size + "&origin=" + window.location.host + "&domain=" + link + "&key=" + gon.grabicon_api_key + ") 0px 5px no-repeat",
         "padding-left": Math.round(size + 4) + "px"
     });
 }
