285f545b47a099c9ae199f9c36367ba3ee149380
Airat Shigapov <contact@airatshigapov.com>
2015-05-22 14:25:02 +0300

    Send digests only to subscribed users

--- app/interactors/weekly_digest.rb
+++ app/interactors/weekly_digest.rb
@@ -3,7 +3,7 @@ class WeeklyDigest
 
   def call
     Group.all.each do |group|
-      group.memberships.not_pending.each do |membership|
+      group.memberships.not_pending.not_opted_out.each do |membership|
         SendWeeklyDigestEmailJob.perform_later(membership_id: membership.id)
       end
     end
