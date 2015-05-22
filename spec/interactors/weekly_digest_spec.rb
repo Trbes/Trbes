a50518bf789b10bbd4b3e58ef306a32dda7d129d
Airat Shigapov <contact@airatshigapov.com>
2015-05-22 19:30:00 +0300

    Fix typo in weekly digest spec

--- spec/interactors/weekly_digest_spec.rb
+++ spec/interactors/weekly_digest_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 describe WeeklyDigest do
-  let(:group) { create(:group, memberships: create_list(:membership, 5)) }
+  let!(:group) { create(:group, memberships: create_list(:membership, 5)) }
 
   it "sends emails to queue" do
     expect(SendWeeklyDigestEmailJob).to receive(:perform_later).exactly(5).times
